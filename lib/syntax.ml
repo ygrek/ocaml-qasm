7f4d03af9b20ab2459c05bb1312a120e6c7f2b12
ygrek <ygrek@autistici.org>
2020-03-16 21:40:09 -0400

    use ppx_deriving

--- lib/syntax.ml
+++ lib/syntax.ml
@@ -1,8 +1,8 @@
 
-type id = string
+type id = string [@@deriving show { with_path = false }]
 
-type binop = Add | Sub | Mul | Div | Pow
-type unop = Sin | Cos | Tan | Exp | Ln | Sqrt | Neg
+type binop = Add | Sub | Mul | Div | Pow [@@deriving show { with_path = false }]
+type unop = Sin | Cos | Tan | Exp | Ln | Sqrt | Neg [@@deriving show { with_path = false }]
 type exp =
 | Int of int
 | Real of float
@@ -10,21 +10,25 @@ type exp =
 | R of id
 | Binop of exp * binop * exp
 | Unaryop of unop * exp
+[@@deriving show { with_path = false }]
 
-type arg = Reg of id | Index of (id * int)
+type arg = Reg of id | Index of (id * int) [@@deriving show { with_path = false }]
 
-type ('param, 'arg) gate = { id : id; params : 'param list option; args : 'arg list; }
+type ('param, 'arg) gate = { id : id; params : 'param list option; args : 'arg list; } [@@deriving show { with_path = false }]
 
 type op = (exp, arg) gate
+[@@deriving show { with_path = false }]
 
 type gate_op =
 | GOp of op
 | GBarrier of id list
+[@@deriving show { with_path = false }]
 
 type qop =
 | Op of op
 | Measure of arg * arg
 | Reset of arg
+[@@deriving show { with_path = false }]
 
 type statement =
 | Gate of ((id, id) gate * gate_op list)
@@ -34,5 +38,6 @@ type statement =
 | Q of qop
 | Barrier of arg list
 | If of (id * int) * qop
+[@@deriving show { with_path = false }]
 
-type program = statement list
+type program = statement list [@@deriving show { with_path = false }]
--- src/main.ml
+++ src/main.ml
@@ -4,4 +4,5 @@ let pr fmt = Printf.ksprintf print_endline fmt
 let () =
   let l = Qasm.Parse.stdin () in
   pr "%d statements" (List.length l);
+  print_endline @@ Qasm.Syntax.show_program l;
   ()
